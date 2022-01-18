0b31cd8235239017dce00defe10275615cbca4a6
abdurrahman al ghifari <abdurrahman@thinkpad>
2022-01-18 12:17:25 +0700

    auto commit - 2022-01-18

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# workstation
+## Machine:
+```
+Linux 5.13.0-7620-generic GNU/Linux
+```
+## Date:
+```
+Sel 18 Jan 2022 12:17:25  WIB
+```
+## Idle:
+```
+ 12:17:25 up  3:25,  1 user,  load average: 1,67, 1,91, 1,95
+```
---
+++ cron.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+# Define your installation directory
+DIRECTORY=/tmp/workstation
+
+PATH_UNAME=$(which uname)
+PATH_DATE=$(which date)
+PATH_UPTIME=$(which uptime)
+
+README=$DIRECTORY/README.md
+
+UNAME=$(${PATH_UNAME} -sro)
+DATE=$(${PATH_DATE})
+UPTIME=$(${PATH_UPTIME})
+
+printf "$echo" > ${README}
+# printf "\`\`\`\n\n" >> ${README}
+printf "## Machine:\n\`\`\`\n$UNAME\n\`\`\`\n" >> ${README}
+printf "## Date:\n\`\`\`\n$DATE\n\`\`\`\n" >> ${README}
+printf "## Idle:\n\`\`\`\n$UPTIME\n\`\`\`\n" >> ${README}
+
+
+cd $DIRECTORY && git add -A && git commit -m "auto commit - `date +\"%Y-%m-%d\"`" && git push origin main
+
+
