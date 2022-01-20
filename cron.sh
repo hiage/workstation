a6ae27d3a776fa153d5966a1e6cd8520f119c0db
abdurrahman al ghifari <abdurrahman@thinkpad>
2022-01-20 08:41:19 +0700

    auto commit - 2022-01-20

--- cron.sh
+++ cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 # Define your installation directory
-DIRECTORY=/tmp/workstation/
-#DIRECTORY=/home/runner/work/workstation/workstation
+#DIRECTORY=/tmp/workstation/
+DIRECTORY=/home/runner/work/workstation/workstation
 
 PATH_UNAME=$(which uname)
 PATH_DATE=$(which date)
@@ -18,7 +18,6 @@ POWEREDBY=$($PATH_ECHO 'Powered by [Github Actions](https://github.com/features/
 
 printf "$echo" > ${README}
 printf "$POWEREDBY\n" >> ${README}
-#printf "$PASS\n" >> ${README}
 bash -c 'echo "[![Auto commit](https://github.com/gyfary/workstation/workflows/Auto%20commit/badge.svg)](https://github.com/gyfary/workstation/actions?query=workflow%3A%22Auto+commit%22)"' >> ${README}
 printf "\n## Machine:\n\`\`\`\n$UNAME\n\`\`\`\n" >> ${README}
 printf "## Date:\n\`\`\`\n$DATE\n\`\`\`\n" >> ${README}
